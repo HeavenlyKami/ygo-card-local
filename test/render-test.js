82b5e1dbf597b416ae629e60ebc72e0c281b97c0
joey <joey.zong@klarrio.com>
2023-02-26 16:32:23 +1100

    the first step

--- test/render-test.js
+++ test/render-test.js
@@ -1,9 +1,12 @@
-const { Card } = require('../packages/node/dist/index.js');
+const { Card,getData } = require('../packages/node/dist/index.js');
 const fs = require('fs');
 
 const DEFAULT_IMAGE_BASE = 'https://gitee.com/ymssx/pics/raw/master/500';
 const OUTPUT_PATH = './demo';
 const MOLD_PATH = './packages/node/dist/mold';
+const INPUT_PATH = './input.txt';
+// sqlite3 YgoText.db < ygo-database/locales/zh-CN/cards.cdb.sql
+const DB_PATH = './YgoText.db';
 const lang = 'cn';
 const testData = [
     {
@@ -166,8 +169,30 @@ async function renderCanvasToImageFile(canvas, file) {
     });
 }
 
-async function renderTest() {
-    testData
+const inputSet = new Set();
+const codes = fs.readFileSync(INPUT_PATH)
+    .toString('utf-8')
+    .split("\n")
+    .map(code => code.trim())
+    .forEach(code =>
+        { if (code.match(/^[0-9]+$/) != null) inputSet.add(code) }
+    )
+const cardInfo = [];
+inputSet.forEach(code => {
+    cardInfo.push(getData(DB_PATH, code))
+})
+
+Promise.all(cardInfo)
+    .then(values => {
+        console.log("Card Info fetched successfully")
+        renderTest(values)
+    })
+
+// const re = getData(DB_PATH, "1861629")
+// re.then(x => console.log(x))
+
+async function renderTest(values) {
+    values
         .map(data => new Card({
             data: Card.transData(data),
             lang: lang,
@@ -180,4 +205,4 @@ async function renderTest() {
         })
 }
 
-renderTest();
+// renderTest();
